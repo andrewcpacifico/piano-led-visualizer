b556bad8ad10d2169d00b16662efc1de66edcedb
Andrew Pacífico <andrewcpacifico@gmail.com>
2020-07-04 18:46:49 -0400

    feat: add midi listener identifying pressed keys

--- src/desktop/js/renderer.js
+++ src/desktop/js/renderer.js
@@ -20,8 +20,23 @@ function createMidiDeviceSelector() {
   });
 }
 
+function startListenMidiDevice() {
+  const selector = document.querySelector('#midi-device-selector');
+  const midiPortId = parseInt(selector.value, 10);
+
+  MidiService.listenDevice(midiPortId);
+}
+
+function registerEvents() {
+  const startButton = document.querySelector('#btn-start');
+  startButton.addEventListener('click', () => {
+    startListenMidiDevice();
+  });
+}
+
 colorPicker.on('color:change', (color) => {
   console.log(color);
 });
 
 createMidiDeviceSelector();
+registerEvents();
--- src/services/midi/index.js
+++ src/services/midi/index.js
@@ -1,8 +1,31 @@
-const midi = require('midi');
+const { Input } = require('midi');
 
-const input = new midi.Input();
+const input = new Input();
+
+const typeMap = {
+  noteOn: 144,
+  noteOff: 128,
+};
 
 const MidiService = {
+  listenDevice(portId) {
+    input.on('message', (deltaTime, message) => {
+      const [type, note] = message;
+      const key = 108 - note;
+
+      if (type === typeMap.noteOn) {
+        console.log(`Pressed key: ${key}`);
+      } else if (type === typeMap.noteOff) {
+        console.log(`Released key: ${key}`);
+      }
+    });
+
+    input.openPort(portId);
+    input.ignoreTypes(false, false, false);
+
+    console.log('Starting to record midi...');
+  },
+
   getDevices() {
     const portCount = input.getPortCount();
     const devices = [];
